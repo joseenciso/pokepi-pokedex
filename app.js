39e6b652a95b4dee2e1f204a9e6fab606b86f317
Jose Enciso <josejefloen@gmail.com>
2022-09-30 11:28:29 +0100

    Changing the project from Pokedex to Cards to gather the information from API

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const POKEAPI = 'https://pokeapi.co/api/v2/';
 const POKEMON = ['ditto/', 'pikachu/'];
 
+/*
 function getCategory( ) {
 	const category = document.getElementById('category');
 	//const categoryName = document.querySelector('category-name');
@@ -19,8 +20,10 @@ function getCategory( ) {
 		getURL( catVal );
 	});
 }
+*/
 
-function getURL( catVal ) {
+function getURL( ) {
+	const catVal = "pokemon/" 
 	//console.log("15 " + POKEAPI+catVal)
 	url = POKEAPI+catVal;
 	//console.log(url);
@@ -34,29 +37,66 @@ function pokedexCount( count ) {
 function pokedexData( pokedex ) {
 	//console.log( pokedex );
 	const pokemons = pokedex.results;
-	let dOptions = document.querySelector(".display-options");
-	
+	let cardsContainer = document.querySelector(".pokemon-cards-container");
+	//console.log(pokedex.results)
 	//console.log(typeof(pokemons));
+
+	/*********************
+		<div class="pokemon-cards-container">
+			<div class="pokedex">
+				<div class="display">
+					<!--h2 id="pokeHeader" class="category-name">Category</!--h2-->
+					<div id="display-options" class="display-options">
+					</div>
+				</div>
+			</div>
+		</div>
+	****************************/
 	
 	for ( let key in pokemons) {
 		fetch(pokemons[key].url)
 		.then( res => res.json() )
 		.then( pokemon => {
-			console.log( pokemon.name.toUppercase )
-			console.log( pokemon.sprites.front_default )
+
+			let capitalName = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+			let divPokeCardCreator = '';
+			let divPokeCard = document.createElement("div");
 			
-			let divPokeBio = document.createElement('div');
-			divPokeBio.className = "pokemonBio";
-			let h3 = document.createElement('h3');
-			h3.innerHTML= pokemon.name;
+			divPokeCard.className = "pokemon-card";
+			divPokeCardCreator = `
+			<div class="card-header">
+				<span class="pokemon-name">${ capitalName }</span>
+				<span>HP ${ pokemon.stats[0].base_stat }</span>
+				</div>
+			</div>
+			<div class="image-container">
+				<img src="${pokemon.sprites.front_default}" class="" />
+			</div>
+
+			<div class="card-skills">
+				<div class="attack">Attack: ${pokemon.stats[1].base_stat}</div>
+				<div class="attack-special">Speacial Attack: ${pokemon.stats[3].base_stat}</div>
+				<div class="defense">Defense: ${pokemon.stats[2].base_stat}</div>
+				<div class="defense-special">Special Defense: ${pokemon.stats[4].base_stat}</div>
+				<div class="speed">Speed: ${pokemon.stats[5].base_stat}</div>
+			</div>
+			`;
 			
-			divPokeBio.appendChild(h3)
-			let img = document.createElement('img');
-			img.src = pokemon.sprites.front_default;
-			divPokeBio.appendChild(img)
-			//console.log(divPokeBio)
-			//console.log(dOptions)
-			dOptions.appendChild(divPokeBio)
+			/*console.log( pokemon.name.charAt(0).toUpperCase())
+			console.log( pokemon.name.slice(1))
+			console.log(capitalName)*/
+			divPokeCard.innerHTML = divPokeCardCreator;
+
+			console.log(pokemon)
+			/*console.log( capitalName )
+			console.log(pokemon.stats[0].stat.name + ": " + pokemon.stats[0].base_stat);
+			console.log(pokemon.stats[1].stat.name + ": " + pokemon.stats[0].base_stat);
+			console.log(pokemon.stats[2].stat.name + ": " + pokemon.stats[0].base_stat);
+			console.log(pokemon.stats[3].stat.name + ": " + pokemon.stats[0].base_stat);
+			console.log(pokemon.stats[4].stat.name + ": " + pokemon.stats[0].base_stat);
+			console.log(pokemon.stats[5].stat.name + ": " + pokemon.stats[0].base_stat);
+			console.log("-------------------------------")
+			*/cardsContainer.appendChild(divPokeCard)
 			//console.log(Object.values(pokemon))
 		})
 		//console.log(pokemons[key].name)
@@ -75,6 +115,7 @@ function urlapi( url ){
 }
 
 document.addEventListener("DOMContentLoaded", ( ) => {
-	getCategory();
+	//getCategory();
+	getURL();
 })
 
